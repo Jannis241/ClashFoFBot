75096ec7bcde11dc67206cd04ee4f7c0cc0ba584
Jannis241 <jannis2401p@gmail.com>
2025-07-28 18:45:59 +0200

    sdjh

--- src/debug.rs
+++ src/debug.rs
@@ -1,12 +1,6 @@
 use crate::prelude::*;
 
 pub fn run_tests() {
-    let options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "Screenshot Tool",
-        options,
-        Box::new(|_cc| Ok(Box::new(ui::ScreenshotApp::default()))),
-    );
     println!("Runnings tests..");
 
     let buildings = image_data_wrapper::get_buildings(Path::new("images/fufu.png"));
