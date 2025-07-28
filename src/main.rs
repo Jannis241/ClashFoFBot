fdef57555f125ac5b5e46cb77f20fb23037697f7
Jesko <jjputzi0410@gmail.com>
2025-07-28 12:22:37 +0200

    prelude fertig

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,8 @@
+mod bot_actions;
+mod image_data_wrapper;
+mod prelude;
+mod screenshot;
+mod settings_manager;
+mod ui;
+
 fn main() {}
---
+++ src/prelude.rs
@@ -0,0 +1,7 @@
+pub use crate::bot_actions::*;
+pub use crate::image_data_wrapper::*;
+pub use crate::screenshot::*;
+pub use crate::settings_manager::*;
+pub use crate::ui::*;
+pub use eframe;
+pub use scrap::{Capturer, Display};
