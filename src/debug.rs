a509479233df41011db2a488f0ccaeadc5ac436e
Jannis241 <jannis2401p@gmail.com>
2025-07-28 21:04:51 +0200

    sdfkjhshfjk

--- src/debug.rs
+++ src/debug.rs
@@ -2,12 +2,12 @@ use crate::prelude::*;
 
 pub fn run_tests() {
     println!("Runnings tests..");
-    let options = eframe::NativeOptions::default();
-    eframe::run_native(
-        "Screenshot Tool",
-        options,
-        Box::new(|_cc| Ok(Box::new(ui::ScreenshotApp::default()))),
-    );
+    // let options = eframe::NativeOptions::default();
+    // eframe::run_native(
+    //     "Screenshot Tool",
+    //     options,
+    //     Box::new(|_cc| Ok(Box::new(ui::ScreenshotApp::default()))),
+    // );
 
     image_data_wrapper::train_model(1);
 
