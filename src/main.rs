ba6e2164890a66a06bd4e809444cec8cb823108a
Jannis241 <jannis2401p@gmail.com>
2025-07-28 11:50:51 +0200

    sdfljkh

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
 fn main() {
     println!("main");
 }
-fn lkdsaöjfsadf
-fn fufu() {}
-
-<<<<<<< HEAD
-=======
-fn dede() {
-    println!("hier ist die dede funktion!");
-}
-
-
-
-
->>>>>>> 9a2bce7 (sdfjklsdhfj)
-fn test3(A)
