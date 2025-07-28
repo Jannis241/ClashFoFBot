a2f8b1e4583b8d91ae132559d6db4e3bc633ddc6
Jannis241 <jannis2401p@gmail.com>
2025-07-28 12:24:02 +0200

    new crates

--- src/prelude.rs
+++ src/prelude.rs
@@ -5,3 +5,7 @@ pub use crate::settings_manager::*;
 pub use crate::ui::*;
 pub use eframe;
 pub use scrap::{Capturer, Display};
+
+pub use std::fs::{copy, read_to_string, remove_file, write, File, OpenOptions};
+pub use std::io::{BufReader, BufWriter, Read, Write};
+pub use std::path::{Path, PathBuf};
