5be02557950c67b95fd6afd9e8c6f1d215611a01
Jannis241 <jannis2401p@gmail.com>
2025-07-28 16:38:54 +0200

    jshd

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use crate::prelude::*;
 mod bot_actions;
+mod debug;
 mod image_data_wrapper;
 mod prelude;
 mod screenshot;
 mod settings_manager;
-mod tests;
 mod ui;
 
 fn main() {
-    tests::run_tests();
+    run_tests();
 }
