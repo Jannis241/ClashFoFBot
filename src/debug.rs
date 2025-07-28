4dba23a6af562e14c6ffe93c1a3523512962658a
Jannis241 <jannis2401p@gmail.com>
2025-07-28 22:44:27 +0200

    J

--- src/debug.rs
+++ src/debug.rs
@@ -2,12 +2,15 @@ use crate::prelude::*;
 
 pub fn run_tests() {
     println!("Runnings tests..");
-    ui::start_ui();
+    // ui::start_ui();
 
-    image_data_wrapper::train_model(10);
+    image_data_wrapper::train_model(50);
+    image_data_wrapper::train_model(50);
+    image_data_wrapper::train_model(50);
+    image_data_wrapper::train_model(50);
 
-    let buildings = image_data_wrapper::get_buildings(Path::new("images/fufu.png"));
-    println!("Buildings: {:?}", buildings);
+    // let buildings = image_data_wrapper::get_buildings(Path::new("images/fufu.png"));
+    // println!("Buildings: {:?}", buildings);
 
     // let screenshot = screener::make_screenshot(0);
     // screenshot.save("images/test.png").unwrap()
