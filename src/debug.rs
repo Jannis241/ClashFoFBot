c97470f0efc8bc9c7a76d774ff6ff0fd26c78673
Jannis241 <jannis2401p@gmail.com>
2025-08-02 00:33:04 +0200

    ofof

--- src/debug.rs
+++ src/debug.rs
@@ -2,6 +2,6 @@ use crate::image_data_wrapper::*;
 use crate::prelude::*;
 
 pub fn run_tests() {
-    image_data_wrapper::create_model("testmodel", DatasetType::Level, YoloModel::YOLOv8n);
-    image_data_wrapper::get_prediction("testmodel", &"images/fufu.jpg");
+    // image_data_wrapper::create_model("testmodel", DatasetType::Level, YoloModel::YOLOv8n);
+    // image_data_wrapper::get_prediction("testmodel", &"images/fufu.jpg");
 }
