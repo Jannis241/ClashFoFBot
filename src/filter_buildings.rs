6f9ae7d24239266334a6c2bda35deda63eec629d
Jannis241 <jannis2401p@gmail.com>
2025-08-09 15:44:05 +0200

    connect walls hinzugefügt (keine ahnung ob es funktioniert)

--- src/filter_buildings.rs
+++ src/filter_buildings.rs
@@ -1,18 +1,75 @@
 use crate::{image_data_wrapper::Building, prelude::*};
 
+fn get_near_walls(wall: &Building, walls: &Vec<Building>, dist: f32) -> Vec<Building> {
+    let mut res = Vec::new();
+    for w in walls {
+        if calc_dist_between_walls(w, wall) <= dist {
+            // todo: verhindern, dass man die wall mit sich selber connected, da die wall selber ja
+            // auch in walls drinne ist.
+            res.push(w.clone());
+        }
+    }
+    return res;
+}
+
+fn center_of_box(bbox: (f32, f32, f32, f32)) -> (f32, f32) {
+    let (x_min, y_min, x_max, y_max) = bbox;
+    ((x_min + x_max) / 2.0, (y_min + y_max) / 2.0)
+}
+
+fn distance(p1: (f32, f32), p2: (f32, f32)) -> f32 {
+    let dx = p2.0 - p1.0;
+    let dy = p2.1 - p1.1;
+    (dx * dx + dy * dy).sqrt()
+}
+
+fn calc_dist_between_walls(wall1: &Building, wall2: &Building) -> f32 {
+    let bounding_box1 = wall1.bounding_box;
+    let bounding_box2 = wall2.bounding_box;
+
+    let center1 = center_of_box(bounding_box1);
+    let center2 = center_of_box(bounding_box2);
+
+    let dist = distance(center1, center2);
+
+    return dist;
+}
+
+fn create_line_between_walls(wall1: &Building, wall2: &Building) -> ((f32, f32), (f32, f32)) {
+    let bounding_box1 = wall1.bounding_box;
+    let bounding_box2 = wall2.bounding_box;
+
+    let center1 = center_of_box(bounding_box1);
+    let center2 = center_of_box(bounding_box2);
+
+    return (center1, center2);
+}
+
 pub fn connect_walls(
     buildings: &Vec<Building>,
     min_dist_to_connect: f32,
-) -> (Vec<Building>, Vec<(f32, f32, f32, f32)>) {
+) -> (Vec<Building>, Vec<((f32, f32), (f32, f32))>) {
     let mut buildings_without_walls = Vec::new();
     let mut walls = Vec::new();
-    let mut wall_lines: Vec<(f32, f32, f32, f32)> = Vec::new();
+    let mut wall_lines: Vec<((f32, f32), (f32, f32))> = Vec::new();
 
     for building in buildings.clone() {
         if building.class_name.as_str() == "mauer" {
             walls.push(building);
         } else {
             buildings_without_walls.push(building);
+        }
+    }
+
+    // todo: doppelete connections verhindern, da man jetzt durch alle mauern geht, wird jede mauer
+    // mit einander mehrmal connected (aber kein bock grade das zu machen)
+    for wall in &walls {
+        let near_walls = get_near_walls(&wall, &walls, min_dist_to_connect);
+
+        for near_wall in near_walls {
+            let line = create_line_between_walls(&wall, &near_wall);
+
+            wall_lines.push(line);
         }
     }
 
