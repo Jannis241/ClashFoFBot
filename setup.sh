5c60a8aee83e53f8b10bb07465b530ebe14e7424
Jannis <jannis2401p@gmail.com>
2025-08-16 20:39:12 +0200

    um strukturierenttijt

--- setup.sh
+++ setup.sh
@@ -1,66 +1,21 @@
-
-
 #!/usr/bin/env bash
-
-
-
-
 
 OS_TYPE="$(uname -s)"
 
-
-
-
-
 case "$OS_TYPE" in
-
-
     Linux*)
-
-
         echo "Linux erkannt"
-
-
-        ./linux_setup   # hier dein Linux-Skript
-
-
+        scripts/linux_setup   # hier dein Linux-Skript
         ;;
-
-
     Darwin*)
-
-
         echo "macOS erkannt"
-
-
         echo "macOS nicht supported (heheha)"
-
-
-        # ./macos_script.sh   # hier dein macOS-Skript
-
-
         ;;
-
-
     CYGWIN*|MINGW*|MSYS*)
-
-
         echo "Windows erkannt"
-
-
-        ./windows_setup.sh   # Windows-Skript
-
-
+        scripts/windows_setup.sh   # Windows-Skript
         ;;
-
-
     *)
-
-
         echo "Unbekanntes System: $OS_TYPE"
-
-
         ;;
-
-
 esac
--- start.sh
+++ start.sh
@@ -1,64 +1,21 @@
 #!/usr/bin/env bash
 
-
-
-
-
 OS_TYPE="$(uname -s)"
 
-
-
-
-
 case "$OS_TYPE" in
-
-
     Linux*)
-
-
         echo "Linux erkannt"
-
-
-        ./linux_start   # hier dein Linux-Skript
-
-
+        scripts/linux_start   # hier dein Linux-Skript
         ;;
-
-
     Darwin*)
-
-
         echo "macOS erkannt"
-
-
         echo "macOS nicht supported (heheha)"
-
-
-        # ./macos_script.sh   # hier dein macOS-Skript
-
-
         ;;
-
-
     CYGWIN*|MINGW*|MSYS*)
-
-
         echo "Windows erkannt"
-
-
-        ./windows_start.sh   # Windows-Skript
-
-
+        scripts/windows_start.sh   # Windows-Skript
         ;;
-
-
     *)
-
-
         echo "Unbekanntes System: $OS_TYPE"
-
-
         ;;
-
-
 esac
