5fb8d50ffd6f7f034a3ab8ba3bf41daa0cdbaa1c
Jannis <jannis2401p@gmail.com>
2025-08-16 20:30:07 +0200

    jsdfjksdflsdkfjl

---
+++ setup.sh
@@ -0,0 +1,66 @@
+
+
+#!/usr/bin/env bash
+
+
+
+
+
+OS_TYPE="$(uname -s)"
+
+
+
+
+
+case "$OS_TYPE" in
+
+
+    Linux*)
+
+
+        echo "Linux erkannt"
+
+
+        ./linux_setup   # hier dein Linux-Skript
+
+
+        ;;
+
+
+    Darwin*)
+
+
+        echo "macOS erkannt"
+
+
+        echo "macOS nicht supported (heheha)"
+
+
+        # ./macos_script.sh   # hier dein macOS-Skript
+
+
+        ;;
+
+
+    CYGWIN*|MINGW*|MSYS*)
+
+
+        echo "Windows erkannt"
+
+
+        ./windows_setup.sh   # Windows-Skript
+
+
+        ;;
+
+
+    *)
+
+
+        echo "Unbekanntes System: $OS_TYPE"
+
+
+        ;;
+
+
+esac
